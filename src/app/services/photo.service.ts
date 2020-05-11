0a2201b29dfb2ec9173eae54760046bdf168a96b
AnnKemp <ann.kemp@scarlet.be>
2020-05-11 15:44:24 +0200

    photo-taking is working! Now the start of the photo-gallery

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -8,6 +8,8 @@ const { Camera, Filesystem, Storage } = Plugins;
   providedIn: 'root'
 })
 export class PhotoService {
+  public photos: Photo[] = [];
+
   constructor() {
   }
   public async addNewToGallery() {
@@ -17,5 +19,14 @@ export class PhotoService {
       source: CameraSource.Camera,
       quality: 100
     });
+    this.photos.unshift({
+      filepath: "soon...",
+      webviewPath: capturedPhoto.webPath
+    });
   }
+}
+interface Photo {
+  filepath: string;
+  webviewPath: string;
+  base64?: string;
 }
