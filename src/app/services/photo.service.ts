5fe0dc237949a5adff032aad883dbcf7554c90b6
AnnKemp <ann.kemp@scarlet.be>
2020-05-12 20:25:18 +0200

    saving the taken pics

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -12,6 +12,41 @@ export class PhotoService {
 
   constructor() {
   }
+  private async savePicture(cameraPhoto: CameraPhoto) {
+      // Convert photo to base64 format, required by Filesystem API to save
+      const base64Data = await this.readAsBase64(cameraPhoto);
+
+    // Write the file to the data directory
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: FilesystemDirectory.Data
+    });
+
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    return {
+      filepath: fileName,
+      webviewPath: cameraPhoto.webPath
+    };
+  }
+  private async readAsBase64(cameraPhoto: CameraPhoto) {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(cameraPhoto.webPath!);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string;
+  }
+  convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
+
   public async addNewToGallery() {
     // Take a photo
     const capturedPhoto = await Camera.getPhoto({
@@ -19,10 +54,14 @@ export class PhotoService {
       source: CameraSource.Camera,
       quality: 100
     });
-    this.photos.unshift({
+    // Save the picture and add it to photo collection
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    this.photos.unshift(savedImageFile);
+
+    /*this.photos.unshift({
       filepath: "soon...",
-      webviewPath: capturedPhoto.webPath
-    });
+      webviewPath: capturedPhoto.webPath*/
+    //});
   }
 }
 interface Photo {
