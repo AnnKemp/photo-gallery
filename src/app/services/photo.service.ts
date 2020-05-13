197f1a34f56e8328336d68f611a1fdbe8f88a9e3
AnnKemp <ann.kemp@scarlet.be>
2020-05-13 10:38:43 +0200

    fotoapp finished, now adjusting to different platforms to display

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -9,6 +9,7 @@ const { Camera, Filesystem, Storage } = Plugins;
 })
 export class PhotoService {
   public photos: Photo[] = [];
+  private PHOTO_STORAGE: string = "photos";
 
   constructor() {
   }
@@ -58,10 +59,34 @@ export class PhotoService {
     const savedImageFile = await this.savePicture(capturedPhoto);
     this.photos.unshift(savedImageFile);
 
-    /*this.photos.unshift({
-      filepath: "soon...",
-      webviewPath: capturedPhoto.webPath*/
-    //});
+    Storage.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos.map(p => {
+        // Don't save the base64 representation of the photo data,
+        // since it's already saved on the Filesystem
+        const photoCopy = { ...p };
+        delete photoCopy.base64;
+
+        return photoCopy;
+      }))
+    });
+  }
+  public async loadSaved() {
+    // Retrieve cached photo array data
+    const photos = await Storage.get({ key: this.PHOTO_STORAGE });
+    this.photos = JSON.parse(photos.value) || [];
+
+    // Display the photo by reading into base64 format
+    for (let photo of this.photos) {
+      // Read each saved photo's data from the Filesystem
+      const readFile = await Filesystem.readFile({
+        path: photo.filepath,
+        directory: FilesystemDirectory.Data
+      });
+
+      // Web platform only: Save the photo into the base64 field
+      photo.base64 = `data:image/jpeg;base64,${readFile.data}`;
+    }
   }
 }
 interface Photo {
